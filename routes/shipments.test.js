a74ee012b5e64a45bf53e538f792c58259c1026e
Ashley Lin <ashleyylin11@gmail.com>
2023-05-16 17:03:20 -0700

    mocking the shipit API

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,30 @@
 "use strict";
 
+// lean towards using fn to mock specific functions
+// jest.mock("../shipItApi");
+// when using jest.fn, you are forced to import the entire object. DONT
+  // destructure it
+  // when using jest.fn, you MUST do this before ANY import statement in the file
+
+let shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
+
+// when to mock?
+  // when you can't control behavior of function (e.g. dice roll)
+  // making requests that are slow or resource intensive (e.g. rate limited API requests)
+
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
+
+  // this is a unit test! you're just testing the invocation of the shipProduct
+  // function when you go to the corresponding endpoint
+
   test("valid", async function () {
+    shipItApi.shipProduct
+      .mockReturnValue(9030)
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,8 +32,10 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 9030 });
   });
+
+  // you wouldn't mock the below because the behavior is PREDICTABLE!!
 
   test("throws error if empty request body", async function () {
     const resp = await request(app)
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,34 @@
 "use strict";
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
 const {
-  shipProduct,
+  shipProduct, SHIPIT_SHIP_URL
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+
+  axiosMock.onPost(`${SHIPIT_SHIP_URL}`)
+    .reply(200, {
+      "receipt": {
+        "itemId": 1001,
+        "name": "Test Tester",
+        "addr": "100 Test St",
+        "zip": "12345-6789",
+        "shipId": 9030
+      }
+    });
+
   const shipId = await shipProduct({
-    productId: 1000,
-    name: "Test Tester",
-    addr: "100 Test St",
-    zip: "12345-6789",
+    "productId": 1001,
+    "name": "Test Tester",
+    "addr": "100 Test St",
+    "zip": "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(9030);
 });
